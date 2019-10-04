47372a152682b95ea36f6dd44aa00ce4ef0b7abe
Bill Transue <transue@gmail.com>
2019-10-04 15:33:24 -0400

    Use hbx_id for searching for people

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -10,7 +10,7 @@ module Api
           params[:person][:first_name] = /^#{Regexp.escape(params[:person][:first_name])}$/ if params[:person].key?(:first_name)
           params[:person][:last_name] = /^#{Regexp.escape(params[:person][:last_name])}$/ if params[:person].key?(:last_name)
 
-          if @person = Person.where(params[:person].permit(:dob, :encrypted_ssn, :first_name, :last_name)).first
+          if @person = Person.where(params[:person].permit(:dob, :hbx_id, :encrypted_ssn, :first_name, :last_name)).first
             render "events/v2/individuals/search", :formats => [ :xml ], :locals => { :individual => @person }
           else
             head :not_found
@@ -24,9 +24,7 @@ module Api
       def has_valid_search_params?
         params.key?(:person) &&
           (params[:person].key?(:ssn) ||
-            (params[:person].key?(:dob) &&
-             params[:person].key?(:first_name) &&
-             params[:person].key?(:last_name)))
+           params[:person].key?(:hbx_id))
       end
 
       def error
